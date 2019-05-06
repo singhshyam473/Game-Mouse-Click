d9a23976aa28869425840697feef5ef01b81fdd6
singhshyam473 <singhshyam473@gmail.com>
2019-05-06 13:23:33 +0530

    a

--- Game/js/game.js
+++ Game/js/game.js
@@ -1,8 +1,8 @@
 var Game = {};
 
 Game.preload = function() {
-  game.load.image("sky", "assets/back.jpg");
-  game.load.image("ground", "assets/platform.png");
+  game.load.image("background", "assets/background.png");
+  game.load.image("ground", "assets/ground.png");
   game.load.image("gameover", "assets/game_over_sign.png");
   game.load.spritesheet("person", "assets/dude.png", 32, 48);
 
@@ -28,17 +28,18 @@ Game.create = function() {
   //game size limit set to 2000 * 600
   game.world.setBounds(0, 0, 2000, 600);
 
-  // game.world.setBounds(0, 0, 1920, 600);
 
   //background image
-  game.add.tileSprite(0, 0, 2000, 600, "sky");
+  game.add.tileSprite(0, 0, 800, 600, "background");
 
   // form a group of platforms so that i can use it any number of time as i want in my game
   platforms = game.add.group();
   platforms.enableBody = true;
 
-  var ground = platforms.create(0, game.world.height - 64, "ground");
-  ground.scale.setTo(5, 2);
+  ground = platforms.create(0, game.world.height - 150, "ground");
+  // ground.scale.x = 1.5;
+  ground.scale.y = 1.5;
+  ground.scale.setTo(3, 1);
   //make platform immovable so that character can stand in the platform
   ground.body.immovable = true;
 
@@ -46,58 +47,62 @@ Game.create = function() {
   signboard = game.add.sprite(1810, game.world.height - 200, "gameover");
   signboard.scale.x = 0.3;
   signboard.scale.y = 0.3;
+  game.add.text(1815, game.world.height - 170, " GameOver ", {
+    fontSize: "17px",
+    fill: "#000"
+  });
 
   //add character sprite to the canvas
-  player = game.add.sprite(0, game.world.height - 112, "person");
-  player.scale.x = 1;
-  player.scale.y = 1;
+  player = game.add.sprite(0, game.world.height - 136, "person");
+  player.scale.x = 1.5;
+  player.scale.y = 1.5;
   //apply the physics to the character i.e. motion, garvity,bounce, collide etc
   game.physics.enable(player, Phaser.Physics.ARCADE);
 
   player.body.collideWorldBounds = true;
 
-  //to make character or player y axis motion stop gravity is max.
-  // player.body.gravity.y = 50000;
 
   //character annimation for walking
   player.animations.add("left", [0, 1, 2, 3], 10, true);
-  player.animations.add("right", [5, 6, 7, 8], 20, true);
+  player.animations.add("right", [5, 6, 7, 8], 10, true);
 
   // background should follows the player so that character should visible in the playing zone i.e. 800*600 canvas
   game.camera.follow(player);
-  game.camera.deadzone = new Phaser.Rectangle(50, 100, 650, 400);
+  // game.camera.deadzone = new Phaser.Rectangle(50,50,700,﻿500);
+  // game.camera.deadzone.setTo(50,50,2000,500);
 
-  game.add.text(1815, game.world.height - 170, " GameOver ", {
-    fontSize: "17px",
-    fill: "#000"
-  });
+  
 };
 
 // var pointer = game.input.activePointer;
 // var isMovingRight = 0;
 // var isMovingLeft = 0;
+
 var move = 0;
-var movey = 0;
+//var movey = 0;
+
+
 Game.update = function() {
   //enable the collision between player and platforms.
   game.physics.arcade.collide(player, platforms);
-  // console.log("player.x: " + Math.floor(player.x))
+
+  console.log("player.x: " + Math.floor(player.x))
   console.log("player.y: " +  Math.floor(player.y))
   // console.log("mx: " + mx)
   console.log("my: " + my)
   console.log("move: "+move)
 
-  if (player.x>mx-3&&player.x<mx+3) {
+  if (player.x>mx-1&&player.x<mx+1) {
     console.log("reached")
     player.body.velocity.x = 0;
     player.animations.stop();
     move = 0;
   }
 
-  if (player.y>my-3&&player.y<my+3) {
-    player.body.velocity.y = 0;
-    movey = 0;
-  }
+  // if (player.y>my-3&&player.y<my+3) {
+  //   player.body.velocity.y = 0;
+  //   movey = 0;
+  // }
 
   if (move==1) {
       if (Math.floor(player.x)<=mx) {
@@ -110,23 +115,23 @@ Game.update = function() {
         player.animations.play("left");
       } 
 
-    if (movey==1) 
-    {
-      if (my<450&&player.y>450) {
-          //move right
-          player.body.velocity.y = -50;
-          // player.animations.play("right");
-      }
-      else {
-        if (Math.floor(player.y)<my) {
-          player.body.velocity.y = 50;
-        }
-      }
-    }
+    // if (movey==1) 
+    // {
+    //   if (my<450&&player.y>450) {
+    //       //move right
+    //       player.body.velocity.y = -50;
+    //       // player.animations.play("right");
+    //   }
+    //   else {
+    //     if (Math.floor(player.y)<my) {
+    //       player.body.velocity.y = 50;
+    //     }
+    //   }
+    // }
 
   }
 
-  //if mouseclick event occur player start to move with a velocity
+  // if mouseclick event occur player start to move with a velocity
   if (game.input.activePointer.leftButton.isDown) {
     mx = Math.floor(game.input.mousePointer.x);
     my = Math.floor(game.input.mousePointer.y);
